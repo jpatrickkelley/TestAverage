4b8249d12a2d07ce06d4d5a0faacf85262655cc7
jpatrickkelley <jpatrickkelly205@gmail.com>
2019-09-06 13:34:19 -0500

    test average su ks

--- TestAverage/Program.cs
+++ TestAverage/Program.cs
@@ -4,9 +4,44 @@ namespace TestAverage
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+
+string[] test = new string[3];
+
+int i = 0;
+
+double sum = 0;
+
+ 
+for (i = 0; i<test.Length; i++)
+
+{
+
+Console.Write("Enter your test score " + (i + 1) + ": ");
+
+test[i] =
+
+Console.ReadLine();
+
 }
+
+
+for (i = 0; i<test.Length; i++)
+
+{
+
+sum = sum +
+
+Convert.ToDouble(test[i].Trim());
+
+}
+
+Console.WriteLine("\nThis is your average = " + (sum / test.Length));
+
+Console.Write("\nPress the [Enter] key to exit.");
+
+Console.ReadLine();
+    }
+    }
+    }
+
